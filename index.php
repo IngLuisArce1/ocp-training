b35f84562244a918ee94297ea49054431e7ea8bf
Heber Romero Tellez <hromerot@hromerot.redhat.com>
2020-09-01 23:56:31 -0500

    hello red hat

---
+++ index.php
@@ -0,0 +1,12 @@
+  
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p></p>
+        <?php echo '<p>Hola, Red Hat!</p>'; ?>
+    </body>
+</html>
